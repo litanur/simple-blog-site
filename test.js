fa384ee2223c2fcbeae43961991e3b344c2454f8
litanur <litanurlaelati@gmail.com>
2021-07-31 20:38:39 +0700

    create greet function

--- test.js
+++ test.js
@@ -1,3 +1,10 @@
-const name = 'mario'
+// const name = 'mario'
+//
+// console.log(name)
 
-console.log(name)
+const greet = (name) => {
+  console.log(`hello ${name}`)
+}
+
+greet('mario')
+greet('yoshi')
